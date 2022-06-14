9476361625a6557fea93d0c56638c2db5dceb6d7
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-06-14 06:37:40 -0700

    Started RISKyBusiness
    Kicked off computer

---
+++ riskyBusiness.py
@@ -0,0 +1,17 @@
+#pip install pillow
+#pip install openpyxl
+
+""" 
+RiskyBusiness will scour the relevant downtime excel sheet on the s/Drive, and create an array of 
+RISK ratings and a matching array of equipment numbers. Those arrays will be sent to LIMBLE via API (seperate function/file)
+ """
+
+# - THIS IS WHAT IS NEXT - ##############
+""" 
+6.14 
+
+
+
+ """
+#########################################
+from openpyxl import load_workbook
