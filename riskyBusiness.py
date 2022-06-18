43606b47d84ba4b74a9f7528e68005a7ffb79e50
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-06-18 06:48:19 -0700

    Built assets.json API
    line 17 on risky has next steps.

--- riskyBusiness.py
+++ riskyBusiness.py
@@ -8,10 +8,50 @@ RISK ratings and a matching array of equipment numbers. Those arrays will be sen
 
 # - THIS IS WHAT IS NEXT - ##############
 """ 
-6.14 
+6.18 -  P067 - Adamatic / SL
+        P068 - Stringline
+        P069 - MainLine
+        P070 - 
+        P071 - BreadLine
+        P072 - MuffinLine
+        Organize info from downtime.xlsx, col D, into 4 categories (above), then apply RISK to those downtime minutes, then update assets with new RISK info via API
 
 
 
  """
 #########################################
 from openpyxl import load_workbook
+import http.client
+import json
+from http.client import HTTPSConnection
+from base64 import b64encode
+
+def getAssets():
+    conn = http.client.HTTPSConnection("api.limblecmms.com", 443)
+    payload = ""
+    userAndPass = b64encode(
+        b"DCHBKYZF5NMXHCV8AG4M1J53DFDONO8Z:WV0KBNNCRLP0SO3CYZMOGFQATYTPG2Y").decode("ascii")
+
+    headers = {
+        'Authorization': 'Basic %s' % userAndPass,
+    }
+    conn.request("GET", "/v2/assets", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    json_output = json.loads(data)
+    #print(json_output)
+
+    for i in json_output:
+        #print(i['name'])
+        riskOut = (i['name'])
+    #END FOR-i
+
+    with open('assets.json', 'w') as f:
+        json.dump(json_output, f)
+
+    return riskOut
+#END getAssets()
+
+getAssets()
